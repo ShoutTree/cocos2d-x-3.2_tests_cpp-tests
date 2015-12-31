1d85568f9d93d9df032d5e7b9ad5f8bd2d51bf90
shouttree <zengxiaoqi@gmail.com>
2015-12-31 16:04:16 +0800

    用bat文件直接运行可执行文件。 这里要注意：在bat文件里面直接运行游戏的exe是不可行的，因为对于cpp-tests工程，当前工作目录必须要是cpp-tests/Resourses/文件夹。 所以这里在run.bat里面将当前工作目录跳转到Resources/文件夹，然后在那里面直接运行exe文件。 对于lua-tests工程来说，当前工作目录必须是lua-tests/文件夹。

---
+++ run.bat
@@ -0,0 +1,4 @@
+REM first goto Resources\ directory, to make the current working directory to be Resources\
+REM we should not direct call Resources\run.bat
+cd Resources
+run_direct.bat
---
+++ run_lua.bat
@@ -0,0 +1,4 @@
+REM first goto lua-tests\ directory, to make the current working directory to be lua-tests\
+REM we should not direct call ..\lua-tests\run_lua.bat
+cd ..\lua-tests\
+run_lua_direct.bat
